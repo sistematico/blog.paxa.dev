140035101dad1cee53d39aa0a9a3010cbb2591a2
Lucas Saliés Brum <sistematico@gmail.com>
2024-05-25 19:13:24 -0400

    Updated Styles

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,15 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from 'astro:content'
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
-});
+  type: 'content',
+  schema: z.object({ // Type-check frontmatter using a schema
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(), // Transform string to Date object
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    published: z.boolean().optional()
+  })
+})
 
-export const collections = { blog };
+export const collections = { blog }
