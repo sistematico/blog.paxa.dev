786eacfa0ec0fa0264afe95d8c53cd191577056e
Lucas Saliés Brum <sistematico@gmail.com>
2024-10-28 06:31:48 -0400

    Added astro-m2dx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,33 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import m2dx from 'astro-m2dx';
 import expressiveCode from 'astro-expressive-code';
+import remarkDirective from 'remark-directive';
 
-const isProd = import.meta.env.PROD;
+const site = import.meta.env.PROD ? 'https://blog.paxa.dev' : 'http://localhost:4321';
+
+/** @type {import('astro-m2dx').Options} */
+const m2dxOptions = {
+  relativeImages: true,
+  styleDirectives: true
+};
 
 // https://astro.build/config
 export default defineConfig({
-  site: isProd ? 'https://blog.paxa.dev' : 'http://localhost:4321',
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), expressiveCode(), mdx()],
+  site,
+  integrations: [
+    tailwind({ applyBaseStyles: false }), 
+    expressiveCode(), 
+    mdx()
+  ],
+  markdown: {
+    remarkPlugins: [
+      remarkDirective,
+      [m2dx, m2dxOptions]
+    ],
+    extendDefaultPlugins: true
+  },
   vite: {
     css: {
       preprocessorOptions: {
